07c1ab37fb6c73d60670b1300232a134c1502f51
Murivhami <tshedzamurivhami@gmail.com>
2025-02-16 08:33:14 -0800

    w9

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,10 +1,11 @@
-#!/bin/bash
+
 
 from django.db import models
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=255)
-    published_date = models.DateField()
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
 
 
     def __str__(self):
