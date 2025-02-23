1dee9c8426af3a7db923283520f805d4fc8a89a7
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-23 19:40:15 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,4 +3,5 @@ from django.db import models
 books.all()
 Author.objects.get(name=author_name), objects.filter(author=author)
 Library.objects.get(name=library_name)
-Librarian.objects.get(library =")
+
+Librarian.objects.get(library =" ")
