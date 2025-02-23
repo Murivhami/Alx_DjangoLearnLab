d051f8397b3ec18564efb9af1ad5aa66c5098e58
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-23 20:03:08 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Book
 
 def book_list(request):
-    # Retrieve all books from the database
-    books = Book.objects.all()
-    
-    # Create a simple text response with book titles and authors
-    book_details = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    
-    # Return the text as the HttpResponse
-    return HttpResponse(f"<pre>{book_details}</pre>")
+    books = Book.objects.all()  # Get all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
