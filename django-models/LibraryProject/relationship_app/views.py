bd0b1a35b74dccee0ba45bcc3be8d78c5f050896
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-23 20:10:05 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,3 +5,8 @@ def book_list(request):
     books = Book.objects.all()  # Get all books from the database
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+from django.views.generic import TemplateView
+from .models import Library
+
+class DetailView(TemplateView):
+    template_name = 'library_detail.html'
