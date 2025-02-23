0051760c64ed23ddb16d1a25bb542c56007fad71
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-23 19:40:59 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,4 +4,4 @@ books.all()
 Author.objects.get(name=author_name), objects.filter(author=author)
 Library.objects.get(name=library_name)
 
-Librarian.objects.get(library =" ")
+Librarian.objects.get(library ="")
