98d523eb1d20be8f3a7840db03e7f31ec19da608
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-24 21:47:13 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,7 +20,7 @@ from django.contrib.auth import login
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/login.html'
+    template_name = 'relationship_app/register.html'
 
 
 
