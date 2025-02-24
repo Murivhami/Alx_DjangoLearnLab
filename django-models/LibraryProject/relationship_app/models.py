c1ecc51a986809b4ea24829ac740ecbdc189e684
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-24 22:20:53 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from relationship_app.apps import UserProfile
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
