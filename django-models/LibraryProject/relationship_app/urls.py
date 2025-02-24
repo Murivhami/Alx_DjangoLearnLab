e32a55d92a12e098c5d05f13bc3eb3465c638077
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-24 21:22:53 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, login
+from .views import list_books, LibraryDetailView, UserCreationForm
 
 urlpatterns = [
     path("",views.list_books, name="list_books"),
