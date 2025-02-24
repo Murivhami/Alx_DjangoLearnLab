41037bf1ed88d2c49eb2bfc2675ec5696c315c20
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-24 22:51:26 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,15 +22,7 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
-
-@user_passes_test(is_admin, login_url='/403/')
-def admin_dashboard(request):
-    return render(request, 'admin_dashboard.html')
 
 
 
