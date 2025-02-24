9a28946a4884b4bd63107f1e4604fd4b46a12593
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-24 22:37:38 +0200

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/views/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/views/relationship_app/admin_view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
 
 @user_passes_test(is_admin)
 def admin_dashboard(request):
