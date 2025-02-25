b76720e976042519cf51996cf53ada31a2cabe80
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-25 10:48:18 +0200

    Update settings.py

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf.apps.BookshelfConfig',
+    'relationshipapp.apps.RelationshipAppConfig',
 ]
 
 MIDDLEWARE = [
