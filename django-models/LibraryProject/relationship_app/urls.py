47d4aae60f052deafaa7d7afb2c97c2aa537b3c0
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-25 17:58:19 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian-dashboard/', librarian_dashboard, name='librarian_dashboard'),
     path('member-dashboard/', member_dashboard, name='member_dashboard'),
-    path('books/add', views.add_book, name='add_book'),
-    path('books/edit', views.edit_book, name='edit_book'),
-    path('books/delete', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/', views.edit_book, name='edit_book'),
+    path('delete_book/', views.delete_book, name='delete_book'),
                                        
     
 
