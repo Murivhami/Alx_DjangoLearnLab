d4f8ee363223ec2c61816660f2191c68bc180b8a
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-25 11:04:36 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -33,3 +33,16 @@ class UserProfile(models.Model):
 
 def __str__(self):
     return self.name
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
