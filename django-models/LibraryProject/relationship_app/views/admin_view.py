c8c7f6850dd817b770593a03095c04609741f9f1
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-25 10:56:51 +0200

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/views/admin_view.py
+++ django-models/LibraryProject/relationship_app/views/admin_view.py
@@ -7,4 +7,5 @@ def is_admin(user):
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html'
+    return render(request, 'admin_view.html')
+
