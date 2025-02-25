08c241bb969de6f575f07aa00a2093852240c30f
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-25 10:45:11 +0200

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/views/admin_view.py
+++ django-models/LibraryProject/relationship_app/views/admin_view.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResposnseForbidden
 
 def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'admin'
 
 @user_passes_test(is_admin, login_url='/403/')
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    if request.user.role = 'admin':
+        return render(request, 'admin_view.html')
+    return HttpResponseForbidden("Denied Access")
