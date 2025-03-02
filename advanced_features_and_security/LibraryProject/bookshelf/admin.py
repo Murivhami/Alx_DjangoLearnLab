8f380fc022cd24aedc0a5b2726178bfe0b159711
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-02 09:20:06 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,5 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
   
-admin.site.register(Book)
+admin.site.register(Book, CustomUser, CustomUserAdmin)
 # Register your models here.
