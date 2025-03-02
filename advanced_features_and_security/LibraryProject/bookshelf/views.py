e9c98795bd9f3a9162b0ec1ea928f233551da1fa
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-02 09:55:00 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,45 +1,11 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import get_object_or_404, redirect, render
-from .models import Document
-from .forms import DocumentForm
+from django.shortcuts import render
+from .models import Book
 
 @login_required
-@permission_required('bookshelf.can_edit_document', raise_exception=True)
-def edit_document(request, doc_id):
-    document = get_object_or_404(Document, id=doc_id)
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, instance=document)
-        if form.is_valid():
-            form.save()
-            return redirect('document_detail', doc_id=document.id)
-    else:
-        form = DocumentForm(instance=document)
 
-    return render(request, 'edit_document.html', {'form': form})
-
-@login_required
-@permission_required('bookshelf.can_create_document', raise_exception=True)
-def create_document(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST)
-        if form.is_valid():
-            document = form.save()
-            return redirect('document_detail', doc_id=document.id)
-    else:
-        form = DocumentForm()
-
-    return render(request, 'create_document.html', {'form': form})
-
-@login_required
-@permission_required('bookshelf.can_delete_document', raise_exception=True)
-def delete_document(request, doc_id):
-    document = get_object_or_404(Document, id=doc_id)
-    
-    if request.method == 'POST':
-        document.delete()
-        return redirect('document_list')
-
-    return render(request, 'confirm_delete.html', {'document': document})
-  
-# Create your views here.
