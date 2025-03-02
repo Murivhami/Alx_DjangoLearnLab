36d974ea80ea4289c70502607c0af2b0b81a25d7
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-02 09:23:03 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -5,5 +5,18 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
   
-admin.site.register(Book, CustomUser, CustomUserAdmin)
+admin.site.register(Book)
+
+#Custom User Model Admin
+from django.contrib import admin
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='photos/', null=True, blank=True )
+
+admin.site.register(CustomUser)
+
+
+
 # Register your models here.
