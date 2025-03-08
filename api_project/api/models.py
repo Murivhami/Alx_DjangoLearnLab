6d6d3f2cabbf5324049ab523c630f379f7d1566f
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-08 18:41:41 +0200

    Update models.py

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+#First model for api app.
 
+from django.db import models
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.CharFielf(max_length=200)
+  
 # Create your models here.
