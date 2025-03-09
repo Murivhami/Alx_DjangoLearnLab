0f31dffe83ec879114571af777b87f95c5af7be6
Murivhami <tshedzamurivhami@gmail.com>
2025-03-09 08:26:04 -0700

    Router

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,16 @@
 #Defining views for the Book model
 
 from rest_framework import generics
+from rest_framework import viewsets
 from . models import Book
 from .serializers import BookSerializer
 
+
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
