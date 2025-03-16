451a86a4cc6a57a23c2c39dfbd61644abc172287
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-16 22:08:28 +0200

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -8,7 +8,7 @@ class BookSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class AuthorSerializer(serializers.ModelSerializer):
-    name = BookSerializer(many=True,read_only=True)
+    name = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Author
         fields = '__all__'
