bdf092fefe78415eedac2a1ac8c3a64b16bb1bad
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-18 18:33:09 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookListView(generics.ListView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookDetailView(generics.DetailView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.CreateView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DeleteView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 # Create your views here.
