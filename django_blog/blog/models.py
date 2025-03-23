b413bb7f4cedea35a9479d9b79dfc539af250388
Tshedza <tshedzamurivhami@gmail.com>
2025-03-23 21:04:55 +0200

    Blog Auth views

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,5 +6,6 @@ class Post(models.Model):
     content = models.CharField(max_length=200)
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    
 
 # Create your models here.
