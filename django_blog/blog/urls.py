d3eb87ed8dfc3e8f6c4e4e5e7146ad26af51c25e
Tshedza <tshedzamurivhami@gmail.com>
2025-03-25 20:59:12 +0200

    W13

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from blog.views import RegisterView, LogoutView, LoginView
+from blog.views import PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name = 'register'),
     path('logout/', LogoutView.as_view(), name = 'logout'),
     path('login/', LoginView.as_view(), name = 'login'),
     path('accounts/', LogoutView.as_view(), name = 'accounts/profile/'),
+    path('posts/', PostListView.as_view(), name = 'post_list'),
+    path('posts/new/', PostCreateView.as_view(), name = 'post_create'),
+    path('posts/<int:pk>', PostDetailView.as_view(), name = 'post_detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name = 'post_update'),
+    path('posts/<int:pk>/delete', PostDeleteView.as_view(), name = 'post_delete'),
 
 ]
+
