cf679f93939b98af3b9997c411e681fcd9d2a3c7
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-03-25 19:39:12 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from blog.views import RegisterView, LogoutView, LoginView
 
 urlpatterns = [
-    path('Register/', RegisterView.as_view(), name = 'register'),
-    path('Logout/', LogoutView.as_view(), name = 'logout'),
-    path('Login/', LoginView.as_view(), name = 'login'),
+    path('register/', RegisterView.as_view(), name = 'register'),
+    path('logout/', LogoutView.as_view(), name = 'logout'),
+    path('login/', LoginView.as_view(), name = 'login'),
     path('accounts/', LogoutView.as_view(), name = 'accounts/profile/'),
 
 ]
