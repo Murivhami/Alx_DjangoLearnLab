5d6150871cc5ba9f5aca1be4b0987629b2402c8d
Tshedza <tshedzamurivhami@gmail.com>
2025-03-25 20:38:57 +0200

    W13

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = '__all__'
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+    def acceptable_title(self):
+        title = self.accepted_data.get('title')
+        if len(title) < 5:
+            raise forms.ValidationError("Title must be at least 5 characters long.")
+        return title
+
+
