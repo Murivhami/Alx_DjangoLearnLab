10ad3671462387f7603cf2624e76cb0ea260b447
Tshedza <tshedzamurivhami@gmail.com>
2025-03-25 20:23:51 +0200

    W13

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -9,6 +9,11 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_view = 'blog/templates/blog/register.html'
+    def form_valid(self, form):
+        
+        user = form.save()  
+        return super().form_valid(form)
+
 
 def custom_login_view(request):
     if request.method == 'POST':
@@ -17,16 +22,16 @@ def custom_login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('profile')  # Redirect to the profile page after login
+            return redirect('profile')  
         else:
-            messages.error(request, 'Invalid username or password')  # Add error message
+            messages.error(request, 'Invalid username or password')  
     return render(request, 'blog/login.html')
 
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 
 def custom_logout_view(request):
-    logout(request)  # Logs out the user
+    logout(request)  # Logs out 
     return redirect('/')
 
 
