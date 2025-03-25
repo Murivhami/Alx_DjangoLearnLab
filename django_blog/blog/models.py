0440887d48cdb551c8f2b42ac230db399192bc30
Tshedza <tshedzamurivhami@gmail.com>
2025-03-25 21:16:10 +0200

    W13

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,5 +8,11 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
