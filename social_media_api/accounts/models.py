7d21bb801bbe73c5b2fd1d8425362b4fab5efa81
Tshedza <tshedzamurivhami@gmail.com>
2025-03-29 20:51:51 +0200

    User Authentication

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,19 @@
+#Create a custom user model that extends Django’s AbstractUser, 
+# adding fields such as bio, profile_picture, 
+# and followers (a ManyToMany field referencing itself, symmetrical=False).
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class Followers(models.Model):
+    pass
+
+class CustomUser(AbstractUser):
+    bio = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to='pics/')
+    followers = models.ManyToManyField('self',related_name='followed_by', symmetrical=False, blank=True)
+
+
+
+
+# Create your models here.
