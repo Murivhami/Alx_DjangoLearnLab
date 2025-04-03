65845cfbbd84e547b91e6454c5fb0a372766ce74
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-04-03 17:20:42 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -11,7 +11,7 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
      path('', include(router.urls)),
      path('feed/', views.user_feed, name='user_feed'),
-     path('posts/<int:pk>/like', views.like_post, name='like_post'),
-     path('posts/<int:pk>/unlike', views.unlike_post, name='unlike_post'),
+     path('<int:pk>/like', views.like_post, name='like_post'),
+     path('<int:pk>/unlike', views.unlike_post, name='unlike_post'),
     
 ]
