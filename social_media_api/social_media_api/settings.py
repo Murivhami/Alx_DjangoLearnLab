89f7f64179d7853a61d48b3be457534dd4fd68bb
Tshedza <tshedzamurivhami@gmail.com>
2025-04-06 21:09:16 +0200

    W15

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -83,13 +83,21 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+from decouple import config
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('mysql'),
+        'USER': config('Tshedzaa'),
+        'PASSWORD': config('@My1livetshe'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
